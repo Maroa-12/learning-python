f3376f97e50a1b8c3947cd736a1e40c6227ca41a
Maroa-12 <drmaroa1@gmail.com>
2025-02-06 18:42:08 +0300

    add

---
+++ WEEK1/Input.py
@@ -0,0 +1,21 @@
+name=input('What is ypour name? ')
+print('Hi' + name)
+name = input('What is your name?')
+favorite_color =input('What is your favourite color?')
+print(name + 'likes' + favorite_color)
+birth_year = input('Birth_year ')
+age = 2019 - int (birth_year)
+print(type(age))
+print(age)
+print(type(age))
+weight_lbs = input('Weight(lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+course = ("Python for beginners")
+print(course)
+course = '''  
+Hi John
+Here is our first email to you
+Thank you
+The support team
+'''
--- WEEK1/hamad.py
+++ WEEK1/hamad.py
@@ -5,27 +5,7 @@ print(price)
 name = ('John Smith')
 age = 20
 is_new = True
-name=input('What is ypour name? ')
-print('Hi' + name)
-name = input('What is your name?')
-favorite_color =input('What is your favourite color?')
-print(name + 'likes' + favorite_color)
-birth_year = input('Birth_year ')
-age = 2019 - int (birth_year)
-print(type(age))
-print(age)
-print(type(age))
-weight_lbs = input('Weight(lbs): ')
-weight_kg = int(weight_lbs) * 0.45
-print(weight_kg)
-course = ("Python for beginners")
-print(course)
-course = '''  
-Hi John
-Here is our first email to you
-Thank you
-The support team
-'''
+
 print(course)
 course = 'Python for Beginners'
 print(course[0])
@@ -124,3 +104,4 @@ while i <= 5:
 
 
 
+    
